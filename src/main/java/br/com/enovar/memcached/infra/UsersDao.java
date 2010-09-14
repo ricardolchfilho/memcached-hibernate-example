7dce3e375e5638650aeef46bd23416d50b9f5eb3
Felipe Benevides <fbenev@gmail.com>
2010-09-14 14:31:04 -0300

    removing some useless fields

--- src/main/java/br/com/enovar/memcached/infra/UsersDao.java
+++ src/main/java/br/com/enovar/memcached/infra/UsersDao.java
@@ -1,12 +1,10 @@
 package br.com.enovar.memcached.infra;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
 import java.util.List;
 
 import javax.persistence.EntityManager;
 
-import net.spy.memcached.MemcachedClient;
 import br.com.caelum.vraptor.ioc.Component;
 import br.com.enovar.memcached.domain.User;
 import br.com.enovar.memcached.domain.UsersRepository;
@@ -16,11 +14,8 @@ public class UsersDao implements UsersRepository {
 
 	private final EntityManager manager;
 	
-	//private final MemcachedClient memcachedClient;
-
 	public UsersDao(EntityManager manager) throws IOException {
 		this.manager = manager;
-		//this.memcachedClient = new MemcachedClient(new InetSocketAddress("localhost", 18000));
 	}
 
 	@Override
